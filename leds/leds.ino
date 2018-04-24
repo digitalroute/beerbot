e83f67237e6bb014e971fed795594d886c31b639
Marcus Johansson <marcus.johansson@digitalroute.com>
2018-04-24 21:39:04 +0200

    some changes to leds

--- leds/leds.ino
+++ leds/leds.ino
@@ -1,107 +1,132 @@
 #include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #define PubNub_BASE_CLIENT WiFiClient
-#define PUBNUB_DEFINE_STRSPN_AND_STRNCASECMP
 #include <PubNub.h>
-#include <aJSON.h>
+#include "secrets.h"
+
+ESP8266WiFiMulti WiFiMulti;
+
+const static char channel[] = "leds"; // channel to use
+
+char message[256];
+
+volatile int pingNow;
+unsigned long oldTime;
 
 #define LED_PIN 13
 
-#define PN_CHANNEL "Test"
-
 Adafruit_NeoPixel handleLEDs = Adafruit_NeoPixel(8, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
-
 
 void setup() {
   Serial.begin(115200);
-  Serial.print("Wait for WiFi... ");
-  WiFi.begin("***", "****");
+  delay(10);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  // We start by connecting to a WiFi network
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(SECRET_SSID, SECRET_SSID_PASSWORD);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Wait for WiFi... ");
+
+  while(WiFiMulti.run() != WL_CONNECTED) {
     Serial.print(".");
+    delay(500);
   }
+
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial.println("");
-  Serial.println("Configure PubNub"); 
-  PubNub.begin("****", "******");
+  delay(500);
 
-  Serial.println("Initialize Handle LED"); 
-  initLedStrip(&handleLEDs);
-}
+  PubNub.begin(SECRET_PUBKEY, SECRET_SUBKEY);
+  Serial.println("PubNub set up");
 
-void initLedStrip(Adafruit_NeoPixel *leds) {
-  Serial.println("Initialize Handle LED"); 
-  leds->begin();
-  leds->show();
+  oldTime = 0;
+  pingNow = 10000; // make sure to send a ping first :)
 }
 
 void loop() {
+
+  ping();
+
+
   colorWipe(handleLEDs.Color(255, 0, 0), 50);
+
   pollPubNub();
 
-  if (WiFi.status() != WL_CONNECTED) {
-     Serial.println("Wifi Error");
-  }
 }
 
 void pollPubNub() {
   WiFiClient *client;
   Serial.println("waiting for a message (subscribe)");
-  client = PubNub.subscribe(PN_CHANNEL);
+
+  PubSubClient *sclient = PubNub.subscribe(channel);
+  if (sclient != 0) {
+    while (sclient->wait_for_data(10)) {
+        Serial.write(sclient->read());
+    }
+    sclient->stop();
+  }
+
+  /*
+  client = PubNub.subscribe(channel);
   if (!client) {
     Serial.println("subscription error");
     delay(1000);
     return;
   } else {
     Serial.println("Message recieved");
-    aJsonClientStream stream(client);
-    Serial.println("Create stream");
-    aJsonObject *msg = aJson.parse(&stream);
-    Serial.println("Parsing");
     client->stop();
-    if (!msg) {
-      Serial.println("parse error");
-      delay(1000);
-      return;
-    } else {
-      dumpMessage(Serial, msg);
-    }
-    aJson.deleteItem(msg);
   }
+  */
 }
 
-void dumpMessage(Stream &s, aJsonObject *msg) {
-  Serial.println("dumpMessage");
-  colorWipe(handleLEDs.Color(255, 0, 0), 50); // Red
-  Serial.println("dumpMessage");
-  aJsonObject *name = aJson.getObjectItem(msg, "text");
-  Serial.println("check");
-  if (name == NULL) {
-    Serial.println("unable to parse message");
+void ping() {
+  if (pingNow >= 30) {
+    Serial.println("Sending ping");
+    createPing(message);
+    publish(message);
+    pingNow = 0;
+  }
+  
+  pingNow++;
+}
+
+void createMessage(char* s, int usage) {
+  sprintf(s, "{\"usage\": \"%d\",\"source\": \"rfid\",\"type\": \"counter\",\"uptime\": \"%d\"}", usage, millis() / 1000);
+}
+
+void createPing(char* s) {
+  sprintf(s, "{\"source\": \"rfid\",\"type\": \"ping\",\"uptime\": \"%d\"}", millis() / 1000);
+}
+
+void publish(char* msg) {
+  WiFiClient *client;
+  
+  client = PubNub.publish(channel, msg);
+
+  if (!client) {
+    //Serial.println("publishing error");
+    delay(1000);
     return;
-  } else {
-    Serial.println("parsed name");
-    Serial.println(name->valuestring);
-    Serial.println("parsed name ddd");
   }
-Serial.println("print");
-  char* string = aJson.print(msg);
-  if (string != NULL) {
-    Serial.println(string);
-    free(string);
-  } 
+  if (PubNub.get_last_http_status_code_class() != PubNub::http_scc_success) {
+    Serial.print("Got HTTP status code error from PubNub, class: ");
+    Serial.print(PubNub.get_last_http_status_code_class(), DEC);
+  }
+  while (client->available()) {
+    Serial.write(client->read());
+  }
+  client->stop();
+  Serial.println("---");
+  
 }
-
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
@@ -117,5 +142,4 @@ void colorPourProgress(uint16_t prog) {
   handleLEDs.numPixels();
   //handleLEDs.setPixelColor(i, handleLEDs.Color(102, 51, 0)); 
 }
-
 
